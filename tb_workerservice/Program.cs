4addd3174f14c88bf48164ceae7ff62e365737e3
tahabutt96 <tahabutt1996@gmail.com>
2023-03-14 11:22:44 +0500

    first commit

---
+++ tb_workerservice/Program.cs
@@ -0,0 +1,11 @@
+using tb_workerservice;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .UseWindowsService()
+    .Build();
+
+await host.RunAsync();
